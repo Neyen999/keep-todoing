189e8c9dc5513c82e6bff832ffb67b3ce9de6362
Neyen Marinelli <neyen_gt@hotmail.com>
2021-12-27 17:09:57 -0300

    Adding local storage

--- src/index.js
+++ src/index.js
@@ -1,44 +1,55 @@
 // Settings variables that i need
 
 const todoContainer      = document.querySelector(".todolist")
-
 const todoInput          = document.querySelector("#search-input")
 const addTodoButton      = document.querySelector("#add--btn")
 
+document.addEventListener("DOMContentLoaded", () => {
+  getLocalTodos()
+  checkTodosLength()
+})
 
 
 addTodoButton.addEventListener("click", () => addTodo())
-todoContainer.addEventListener("click", (e) => deleteTodo(e))
+
+
+todoContainer.addEventListener("click", (e) => {
+  deleteTodo(e)
+  checkTodo(e)
+})
 
 const addTodo = () => {
   // Create list element
+      const todo            = document.createElement("li")      // list element container
+      const todoDescription = document.createElement("p")       // list element description
+      const todoButtonsCont = document.createElement("div")     // buttons container
+      const deleteTodoBtn   = document.createElement("button")  // delete button
+      deleteTodoBtn.classList.add("fas", "fa-minus-circle")
+      const checkTodoBtn    = document.createElement("button")  // check button
+      checkTodoBtn.classList.add("fas", "fa-check-circle")
 
-  const todo            = document.createElement("li")      // list element container
-  const todoDescription = document.createElement("p")       // list element description
-  const todoButtonsCont = document.createElement("div")     // buttons container
-  const deleteTodoBtn   = document.createElement("button")  // delete button
-  deleteTodoBtn.classList.add("fas", "fa-minus-circle")
-  const checkTodoBtn    = document.createElement("button")  // check button
-  checkTodoBtn.classList.add("fas", "fa-plus-circle")
+      // Add the input text on the description
 
-  // Add the input text on the description
+      todoDescription.innerText = todoInput.value
 
-  todoDescription.innerText = todoInput.value
+      // Add todos to localStorage
 
-  // Append the buttons on the div
+      setLocalStorageTodo(todoInput.value)
+      checkTodosLength()
 
-  todoButtonsCont.append(checkTodoBtn, deleteTodoBtn)
+      // Append the buttons on the div
 
-  // Append the description and the buttons container on the li
+      todoButtonsCont.append(checkTodoBtn, deleteTodoBtn)
 
-  todo.append(todoDescription, todoButtonsCont)
+      // // Append the description and the buttons container on the li
 
-  // Now append the li element on the ul
-  todoContainer.appendChild(todo)
+      todo.append(todoDescription, todoButtonsCont)
+
+      // // Now append the li element on the ul
+      todoContainer.appendChild(todo)
 
 
-  todoInput.value = ""
-
+      todoInput.value = ""
 }
 
 const deleteTodo = (e) => {
@@ -46,6 +57,114 @@ const deleteTodo = (e) => {
   const item = e.target
 
   // if is the delete button then delete the li element.
-  if (item.classList.contains("fa-minus-circle")) item.parentElement.parentElement.remove()
-  
+  if (item.classList.contains("fa-minus-circle")) {
+    removeLocalTodos(item.parentElement.parentElement)
+    checkTodosLength()
+    item.parentElement.parentElement.remove()
+  }
+}
+
+const checkTodo = (e) => {
+  // get the target being clicked.
+  const item = e.target
+
+  // if is the delete button then check the li element.
+  if (item.classList.contains("fa-check-circle")) {
+    const liElement = item.parentElement.parentElement
+    liElement.classList.add("checked")
+  }
+}
+
+// Local Storage Settings
+
+ /*
+  1- Tengo que meter los todos al local storage,
+  2- Tengo que hacer que los todos que se muestran en pantalla sean los consumidos por el local storage
+ */
+
+const setLocalStorageTodo = (todo) => {
+  let todos; 
+
+  if (localStorage.getItem('todos') === null) {
+    todos = [];
+  } else {
+    todos = JSON.parse(localStorage.getItem('todos'))
+  }
+
+  todos.push(todo)
+
+  localStorage.setItem('todos', JSON.stringify(todos))
+
+}
+
+const getLocalTodos = () => {
+  let todos;
+
+  if (localStorage.getItem('todos') === null) {
+    todos = [];
+  } else {
+    todos = JSON.parse(localStorage.getItem('todos'))
+  }
+
+  todos.map(todo => {
+    // Create list element
+
+    const todoEl          = document.createElement("li")      // list element container
+    const todoDescription = document.createElement("p")       // list element description
+    const todoButtonsCont = document.createElement("div")     // buttons container
+    const deleteTodoBtn   = document.createElement("button")  // delete button
+    deleteTodoBtn.classList.add("fas", "fa-minus-circle")
+    const checkTodoBtn    = document.createElement("button")  // check button
+    checkTodoBtn.classList.add("fas", "fa-check-circle")
+
+    // Add the input text on the description
+
+    todoDescription.innerText = todo
+
+
+    // Append the buttons on the div
+
+    todoButtonsCont.append(checkTodoBtn, deleteTodoBtn)
+
+    // Append the description and the buttons container on the li
+
+    todoEl.append(todoDescription, todoButtonsCont)
+
+    // Now append the li element on the ul
+    todoContainer.appendChild(todoEl)
+  })
+}
+
+const removeLocalTodos = (todo) => {
+  let todos;
+
+  if (localStorage.getItem('todos') === null) {
+    todos = [];
+  } else {
+    todos = JSON.parse(localStorage.getItem('todos'))
+  }
+
+  const todoIndex = todo.children[0].innerText;
+
+  todos.splice(todoIndex, 1)
+
+  localStorage.setItem('todos', JSON.stringify(todos))
+
+}
+
+const checkTodosLength = () => {
+  console.log("this is happening")
+  let todos;
+
+  if (localStorage.getItem('todos') === null) {
+    todos = [];
+  } else {
+    todos = JSON.parse(localStorage.getItem('todos'))
+  }
+
+  if (todos.length > 5) {
+    todoContainer.style.overflowY = "scroll"
+  } else {
+    todoContainer.style.overflowY = "hidden"
+  }
 }
